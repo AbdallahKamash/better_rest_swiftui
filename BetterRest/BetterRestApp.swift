8105f9fc3c21f1b6adcfd05f4d26d33914338559
AbdallahKamash <abdallah-kamash@outlook.com>
2025-11-26 06:14:58 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abdallah Kamash on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,16 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by Abdallah Kamash on 26/11/2025.
+//
+
+import Testing
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
